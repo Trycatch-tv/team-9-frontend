e3912046395df3cfda7cd28293a68a93a53a8f4e
jaimesanchezgalvisacct <jaime.sanchez@acctglobal>
2023-04-13 23:31:22 +0200

    feat: add new routes and fix layout home

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { AppRouter } from "./router/AppRouter";
+import { AuthProvider } from "./context/AuthProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AppRouter/>
-    </BrowserRouter>
+      <AuthProvider />
   </React.StrictMode>
 );
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,21 +1,31 @@
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import { HomePage } from "../pages/HomePage"
+import { HomePage } from "../pages/HomePage";
 import { NotFound404 } from "../pages/NotFound404";
 import { LoginPage } from "../pages/LoginPage";
 import { ReservationPage } from "../pages/ReservationPage";
 import { AdminPage } from "../pages/AdminPage";
 import { AboudPage } from "../pages/AboudPage";
+import { LogoutApp } from "../components/LogoutApp";
+import { PrivateRouter } from "./PrivateRouter";
+import { PublicRouter } from "./PublicRouter";
 
 export const AppRouter = () => {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage/>}/>
-      <Route path="/reservation" element={<ReservationPage />} />
-      <Route path="/admin" element={<AdminPage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/aboud" element={<AboudPage />} />
-      {/* <Route path="/*" element={<NotFound404 />} /> */}
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<PublicRouter />}>
+          <Route index element={<HomePage />} />
+          <Route path="/reservation" element={<ReservationPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/aboud" element={<AboudPage />} />
+          {/* <Routes path="/*" element={<NotFound404 />} /> */}
+        </Route>
+        <Route path="/private" element={<PrivateRouter />}>
+          <Route index element={<AdminPage />} />
+          <Route path="/private/logout" element={<LogoutApp />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 };
