4c50a82952844bbf17f3be08a8f6d687758d367b
yamir caro <yamircaro@gmail.com>
2023-04-13 11:44:45 -0500

    feat:add new navbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { NavBar } from "./components/NavBar";
 import { AppRouter } from "./router/AppRouter";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
+    <NavBar/>
       <AppRouter/>
     </BrowserRouter>
   </React.StrictMode>
