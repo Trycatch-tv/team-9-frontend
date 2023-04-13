025ea59d4a129359d7bd77e8a3c4fa34c52a708a
jaimesanchezgalvisacct <jaime.sanchez@acctglobal>
2023-04-14 00:38:30 +0200

    feat: add new layout

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { NavBar } from "./components/NavBar";
-import { AppRouter } from "./router/AppRouter";
 import { AuthProvider } from "./context/AuthProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-      <AuthProvider />
+    <AuthProvider />
   </React.StrictMode>
 );
